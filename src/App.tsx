a0052848707b18575f85ae24d23405537c90e478
subaa P <subaap86@gmail.com>
2023-04-05 14:44:59 +0530

    chore: button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./component/Button/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button varient="secondary" children="button" />
+    </div>
+  );
 }
 
 export default App;
