ead3b4158983d40168958ae3eae199bf2b8aaaa5
Arunram2000 <70103361+Arunram2000@users.noreply.github.com>
2023-04-11 12:27:09 +0530

    chore:stakeplans

--- src/components/StakePlans/StakePlans.ts
+++ src/components/StakePlans/StakePlans.ts
@@ -2,7 +2,7 @@ export const IStakePlans = [
   {
     heading: 'SILVER',
     title: '0.5% For 30 Days',
-    price: '$10-$1000',
+    price: '$10 - $1000',
     dailyPrecentage: 'Daily 0.5%',
     profits: 'Profits 115%',
     depositStatus: 'Deposit Included',
@@ -10,7 +10,7 @@ export const IStakePlans = [
   {
     heading: 'GOLD',
     title: '1% For 30 Days',
-    price: '$1001-$10000',
+    price: '$1001 - $10000',
     dailyPrecentage: 'Daily 1%',
     profits: 'Profits 130%',
     depositStatus: 'Deposit Included',
@@ -18,7 +18,7 @@ export const IStakePlans = [
   {
     heading: 'PLANTINUM',
     title: '2% For 30 Days',
-    price: '$10001-$50000',
+    price: '$10001 - $50000',
     dailyPrecentage: 'Daily 2%',
     profits: 'Profits 160%',
     depositStatus: 'Deposit Included',
@@ -26,7 +26,7 @@ export const IStakePlans = [
   {
     heading: 'VIP',
     title: '3% For 30 Days',
-    price: '$50001-$100000',
+    price: '$50001 - $100000',
     dailyPrecentage: 'Daily 3%',
     profits: 'Profits 190%',
     depositStatus: 'Deposit Included',
