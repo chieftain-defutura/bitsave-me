15b0b63ac39c745b2ae255c9e11bec634e82b0b2
Arunram2000 <70103361+Arunram2000@users.noreply.github.com>
2023-04-22 18:05:46 +0530

    chore:updated to mainnet

--- src/utils/Connector/Connector.tsx
+++ src/utils/Connector/Connector.tsx
@@ -1,8 +1,8 @@
 import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { configureChains, createClient } from 'wagmi'
-import { bscTestnet } from 'wagmi/chains'
+import { bsc } from 'wagmi/chains'
 
-export const chains = [bscTestnet]
+export const chains = [bsc]
 export const projectId = 'bf9397e41bf0ab99a492296a2957db54'
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })])
